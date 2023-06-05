8ab5c022e9a27f8e4e02627b21075da6dbe38700
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-05 11:50:58 -0400

    test: update log_ride test 1

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new('Kenny', 30)
+    @biker2 = Biker.new('Athena', 15)
     @ride1 = Ride.new({ name: 'Walnut Creek Trail', distance: 10.7, loop: false, terrain: :hills })
     @ride2 = Ride.new({ name: 'Town Lake', distance: 14.9, loop: true, terrain: :gravel })
   end
@@ -30,10 +31,15 @@ RSpec.describe Biker do
 
   describe '#log_ride' do
     it 'can log a ride' do
+      @biker.learn_terrain(:gravel)
+      @biker.learn_terrain(:hills)
+
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
       @biker.log_ride(@ride2, 60.9)
       @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.rides).to eq({ @ride1 => 91.1, @ride2 => 60.9 })
     end
   end
 end
