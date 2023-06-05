eefa16669dc325e360aee2f0a186296abeee1f1a
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-05 12:19:35 -0400

    fix: updated learn_terrain! name and added biker2 personal_record tests

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -21,18 +21,18 @@ RSpec.describe Biker do
     end
   end
 
-  describe '#learn_terrain' do
+  describe '#learn_terrain!' do
     it 'can learn terrain' do
-      @biker.learn_terrain(:gravel)
-      @biker.learn_terrain(:hills)
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
       expect(@biker.acceptable_terrain).to eq(%i[gravel hills])
     end
   end
 
   describe '#log_ride' do
     it 'can log a ride' do
-      @biker.learn_terrain(:gravel)
-      @biker.learn_terrain(:hills)
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
 
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
@@ -44,7 +44,7 @@ RSpec.describe Biker do
 
     it 'will not log a ride if the ride\'s terrain does not match their acceptable terrain' do
       # Not learning :gravel
-      @biker.learn_terrain(:hills)
+      @biker.learn_terrain!(:hills)
 
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
@@ -65,8 +65,8 @@ RSpec.describe Biker do
     end
 
     it 'will not log a ride if distance exceeds maximum distance of rider' do
-      @biker2.learn_terrain(:gravel)
-      @biker2.learn_terrain(:hills)
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
 
       @biker2.log_ride(@ride1, 95) # This exceeds the maximum distance of biker2
       @biker2.log_ride(@ride2, 65)
@@ -77,8 +77,8 @@ RSpec.describe Biker do
 
   describe '#personal_record' do
     it 'can return the shortest time for a ride' do
-      @biker.learn_terrain(:gravel)
-      @biker.learn_terrain(:hills)
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
 
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
@@ -87,6 +87,17 @@ RSpec.describe Biker do
 
       expect(@biker.personal_record(@ride1)).to eq(91.1)
       expect(@biker.personal_record(@ride2)).to eq(60.9)
+    end
+
+    it 'can return the shortest time for a ride based on terrain rules' do
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
+      @biker2.log_ride(@ride1, 95.0) # Distance is too far
+      @biker2.log_ride(@ride2, 65.0) # Biker knows this terrain and distance is within range
+
+      expect(@biker2.rides).to eq({ @ride2 => [65.0] })
+      expect(@biker2.personal_record(@ride2)).to eq(65.0)
+      expect(@biker2.personal_record(@ride1)).to eq(false)
     end
   end
 end
