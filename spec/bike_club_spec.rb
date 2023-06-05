fd283ddf0a4ff2a96dc0cefb113412dc95898903
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-05 12:46:29 -0400

    test: add biker_with_most_rides test

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -3,8 +3,14 @@ require 'spec_helper'
 RSpec.describe BikeClub do
   before(:each) do
     @bike_club1 = BikeClub.new('Touring with Turing')
+    @bike_club2 = BikeClub.new('Bike Party')
     @biker1 = Biker.new('Kenny', 30)
     @biker2 = Biker.new('Athena', 15)
+    @ride1 = Ride.new({ name: 'Walnut Creek Trail', distance: 10.7, loop: false, terrain: :hills })
+    @biker1.learn_terrain!(:gravel)
+    @biker1.learn_terrain!(:hills)
+    @biker2.learn_terrain!(:gravel)
+    @biker2.learn_terrain!(:hills)
   end
 
   describe '#initialize' do
@@ -20,6 +26,22 @@ RSpec.describe BikeClub do
       @bike_club1.add_biker(@biker1)
 
       expect(@bike_club1.bikers).to eq([@biker1])
+    end
+  end
+
+  describe '#biker_with_most_rides' do
+    it 'can return the biker with the most rides' do
+      @bike_club1.add_biker(@biker1)
+      @bike_club1.add_biker(@biker2)
+      
+      @biker1.log_ride(@ride1, 92.5)
+      @biker1.log_ride(@ride1, 91.1)
+      @biker1.log_ride(@ride1, 90.0)
+
+      @biker2.log_ride(@ride1, 93.0)
+      @biker2.log_ride(@ride1, 92.9)
+
+      expect(@bike_club1.biker_with_most_rides).to eq('Kenny')
     end
   end
 end
