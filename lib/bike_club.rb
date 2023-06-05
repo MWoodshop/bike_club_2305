03d44b9bcd2844dd6eb1e28346a05db4fabf91af
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-05 13:08:41 -0400

    feat: add tie test for biker_with_most_rides method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -10,7 +10,18 @@ class BikeClub
     @bikers << biker
   end
 
+  # def biker_with_most_rides
+  #   @bikers.max_by { |biker| biker.rides.values.flatten.count }.name
+  # end
+
   def biker_with_most_rides
-    @bikers.max_by { |biker| biker.rides.values.flatten.count }.name
+    max_rides = @bikers.map { |biker| biker.rides.values.flatten.count }.max
+    bikers_with_most_rides = @bikers.select { |biker| biker.rides.values.flatten.count == max_rides }
+
+    if bikers_with_most_rides.size == 1
+      bikers_with_most_rides.first.name
+    else
+      bikers_with_most_rides.map(&:name).join(' and ') + ' are tied'
+    end
   end
 end
