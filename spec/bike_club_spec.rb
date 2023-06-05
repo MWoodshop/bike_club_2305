bd389ad718152671bf0940c5025939b6e2ded66f
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-05 13:38:51 -0400

    test: updates tests to check for two way and three way ties.

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -48,7 +48,19 @@ RSpec.describe BikeClub do
       expect(@bike_club1.biker_with_most_rides).to eq('Kenny')
     end
 
-    it 'can return a tie' do
+    it 'can return a two way tie' do
+      @bike_club1.add_biker(@biker1)
+      @bike_club1.add_biker(@biker2)
+
+      @biker1.log_ride(@ride1, 92.5)
+      @biker1.log_ride(@ride1, 91.1)
+
+      @biker2.log_ride(@ride1, 93.0)
+      @biker2.log_ride(@ride1, 92.9)
+
+      expect(@bike_club1.biker_with_most_rides).to eq('Kenny and Athena are tied')
+    end
+    it 'can return a three way tie' do
       @bike_club1.add_biker(@biker1)
       @bike_club1.add_biker(@biker2)
       @bike_club1.add_biker(@biker3)
@@ -81,8 +93,48 @@ RSpec.describe BikeClub do
       @biker2.log_ride(@ride2, 59.8)
       @biker2.log_ride(@ride2, 60.4)
 
-      expect(@bike_club1.fastest_biker_for_ride(@ride1).first.name).to eq('Kenny')
-      expect(@bike_club1.fastest_biker_for_ride(@ride2).first.name).to eq('Athena')
+      expect(@bike_club1.fastest_biker_for_ride(@ride1)).to eq('Kenny')
+      expect(@bike_club1.fastest_biker_for_ride(@ride2)).to eq('Athena')
+    end
+
+    it 'can return a two way tie' do
+      @bike_club1.add_biker(@biker1)
+      @bike_club1.add_biker(@biker2)
+
+      @biker1.log_ride(@ride1, 92.5)
+      @biker1.log_ride(@ride1, 92.5)
+      @biker1.log_ride(@ride2, 60.1)
+      @biker1.log_ride(@ride2, 61.3)
+
+      @biker2.log_ride(@ride1, 92.5)
+      @biker2.log_ride(@ride1, 92.5)
+      @biker2.log_ride(@ride2, 60.1)
+      @biker2.log_ride(@ride2, 61.3)
+
+      expect(@bike_club1.fastest_biker_for_ride(@ride1)).to eq('Kenny and Athena are tied')
+    end
+
+    it 'can return a three way tie' do
+      @bike_club1.add_biker(@biker1)
+      @bike_club1.add_biker(@biker2)
+      @bike_club1.add_biker(@biker3)
+
+      @biker1.log_ride(@ride1, 92.5)
+      @biker1.log_ride(@ride1, 92.5)
+      @biker1.log_ride(@ride2, 60.1)
+      @biker1.log_ride(@ride2, 61.3)
+
+      @biker2.log_ride(@ride1, 92.5)
+      @biker2.log_ride(@ride1, 92.5)
+      @biker2.log_ride(@ride2, 60.1)
+      @biker2.log_ride(@ride2, 61.3)
+
+      @biker3.log_ride(@ride1, 92.5)
+      @biker3.log_ride(@ride1, 92.5)
+      @biker3.log_ride(@ride2, 60.1)
+      @biker3.log_ride(@ride2, 61.3)
+
+      expect(@bike_club1.fastest_biker_for_ride(@ride1)).to eq('Kenny and Athena and Steve are tied')
     end
   end
 end
