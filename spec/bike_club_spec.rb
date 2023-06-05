8e455929777bb1708fe08b292d7b9a817ed80f1b
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-05 12:33:15 -0400

    test: add_biker test

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -3,11 +3,23 @@ require 'spec_helper'
 RSpec.describe BikeClub do
   before(:each) do
     @bike_club1 = BikeClub.new('Touring with Turing')
+    @biker1 = Biker.new('Kenny', 30)
+    @biker2 = Biker.new('Athena', 15)
   end
 
   describe '#initialize' do
     it 'can initialize' do
       expect(@bike_club1).to be_an_instance_of(BikeClub)
+    end
+  end
+
+  describe '#add_biker' do
+    it 'can add bikers' do
+      expect(@bike_club1.bikers).to eq([])
+
+      @bike_club1.add_biker(@biker1)
+
+      expect(@bike_club1.bikers).to eq([@biker1])
     end
   end
 end
