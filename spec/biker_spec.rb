9a04be50c7eb79387ba680a06f48a7dd5665b36c
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-05 12:01:24 -0400

    test: add terrain test to log_ride

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -41,5 +41,17 @@ RSpec.describe Biker do
 
       expect(@biker.rides).to eq({ @ride1 => [92.5, 91.1], @ride2 => [60.9, 61.6] })
     end
+
+    it 'will not log a ride if the ride\'s terrain does not match their acceptable terrain' do
+      # Not learning :gravel
+      @biker.learn_terrain(:hills)
+
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.rides).to eq({ @ride1 => [92.5, 91.1] })
+    end
   end
 end
