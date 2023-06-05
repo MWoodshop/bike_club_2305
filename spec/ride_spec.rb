1ae0ad9837cc3b5db3e4842877a7ab9acab37342
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-05 11:15:45 -0400

    test: add before(:each) and return correct attributes

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe Ride do
+  before(:each) do
+    @ride1 = Ride.new({ name: 'Walnut Creek Trail', distance: 10.7, loop: false, terrain: :hills })
+  end
   it 'can initialize' do
-    ride1 = Ride.new({ name: 'Walnut Creek Trail', distance: 10.7, loop: false, terrain: :hills })
+    expect(@ride1).to be_an_instance_of(Ride)
+  end
 
-    expect(ride1).to be_an_instance_of(Ride)
+  it 'returns correct attributes' do
+    expect(@ride1).name to eq('Walnut Creek Trail')
+    expect(@ride1).distance to eq(10.7)
+    expect(@ride1).loop to eq(false)
+    expect(@ride1).terrain to eq(:hills)
   end
 end
